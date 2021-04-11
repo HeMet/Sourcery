f3436b52d8c58569ece9dc0d7962e12edb9faebc
Hemet <hemet.mail@gmail.com>
2021-04-11 15:06:26 +0400

    Take into account what CRLF in TemplateAnnotationsParserSpec

--- SourceryTests/Parsing/Helpers/TemplateAnnotationsParserSpec.swift
+++ SourceryTests/Parsing/Helpers/TemplateAnnotationsParserSpec.swift
@@ -20,44 +20,52 @@ class TemplateAnnotationsParserSpec: QuickSpec {
         describe("InlineParser") {
             context("without indentation") {
                 let source =
-                        "// sourcery:inline:Type.AutoCoding\n" +
+                        ("// sourcery:inline:Type.AutoCoding\n" +
                         "var something: Int\n" +
-                        "// sourcery:end\n"
+                        "// sourcery:end\n").withPlatformLineTerminator
 
                 let result = TemplateAnnotationsParser.parseAnnotations("inline", contents: source)
 
                 it("tracks it") {
                     let annotatedRanges = result.annotatedRanges["Type.AutoCoding"]
+                    #if os(Windows)
+                    expect(annotatedRanges?.map { $0.range }).to(equal([NSRange(location: 36, length: 20)]))
+                    #else
                     expect(annotatedRanges?.map { $0.range }).to(equal([NSRange(location: 35, length: 19)]))
+                    #endif
                     expect(annotatedRanges?.map { $0.indentation }).to(equal([""]))
                 }
 
                 it("removes content between the markup") {
                     expect(result.contents).to(equal(
-                        "// sourcery:inline:Type.AutoCoding\n" +
-                        "// sourcery:end\n"
+                        ("// sourcery:inline:Type.AutoCoding\n" +
+                        "// sourcery:end\n").withPlatformLineTerminator
                     ))
                 }
             }
 
             context("with indentation") {
                 let source =
-                        "    // sourcery:inline:Type.AutoCoding\n" +
+                        ("    // sourcery:inline:Type.AutoCoding\n" +
                         "    var something: Int\n" +
-                        "    // sourcery:end\n"
+                        "    // sourcery:end\n").withPlatformLineTerminator
 
                 let result = TemplateAnnotationsParser.parseAnnotations("inline", contents: source)
 
                 it("tracks it") {
                     let annotatedRanges = result.annotatedRanges["Type.AutoCoding"]
+                    #if os(Windows)
+                    expect(annotatedRanges?.map { $0.range }).to(equal([NSRange(location: 40, length: 24)]))
+                    #else
                     expect(annotatedRanges?.map { $0.range }).to(equal([NSRange(location: 39, length: 23)]))
+                    #endif
                     expect(annotatedRanges?.map { $0.indentation }).to(equal(["    "]))
                 }
 
                 it("removes content between the markup") {
                     expect(result.contents).to(equal(
-                        "    // sourcery:inline:Type.AutoCoding\n" +
-                        "    // sourcery:end\n"
+                        ("    // sourcery:inline:Type.AutoCoding\n" +
+                        "    // sourcery:end\n").withPlatformLineTerminator
                     ))
                 }
             }
